c2f7478a5697170a2fdb5be736a03fddb9da74cf
dalemusser <dale@dalemusser.com>
2025-06-07 15:38:17 -0700

    feat(render): add template renderer with HTMX support

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
-        "net/http"
+	"net/http"
 
 	"github.com/dalemusser/gowebcore/config"
 	"github.com/dalemusser/gowebcore/logger"
+	"github.com/dalemusser/gowebcore/render"
 	"github.com/dalemusser/gowebcore/server"
 
 	"github.com/go-chi/chi/v5"
@@ -15,17 +16,20 @@ type cfg struct{ config.Base }
 
 func main() {
 	var c cfg
-	_ = config.Load(&c, config.WithEnvPrefix("EX")) // ignore err for brevity
-
+	_ = config.Load(&c, config.WithEnvPrefix("EX"))
 	logger.Init(c.LogLevel)
 
 	r := chi.NewRouter()
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello from gowebcore\n"))
+		render.Render(w, r, "home.html", nil)
+	})
+	r.Get("/fragment", func(w http.ResponseWriter, r *http.Request) {
+		render.Render(w, r, "fragment.html", map[string]any{
+			"Now": "right now",
+		})
 	})
 
 	srv := server.New(c.Base, r)
-	if err := server.Graceful(context.Background(), srv); err != nil {
-		logger.Instance().Error("server error", "err", err)
-	}
+	_ = server.Graceful(context.Background(), srv)
 }
